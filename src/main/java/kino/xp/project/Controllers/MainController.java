5a2344e81cefea1a126e47dcba22d638b987ead3
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-10-02 21:41:58 +0200

    movieIndex

--- src/main/java/kino/xp/project/Controllers/MainController.java
+++ src/main/java/kino/xp/project/Controllers/MainController.java
@@ -1,13 +1,23 @@
 package kino.xp.project.controllers;
 
 
+import kino.xp.project.Service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    MovieService movieService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("movieList", movieService.listMovies());
+
         return "index";
     }
 }
