20a92d39191775cfeec112306ee1f049a91c18d1
caerKEA <58211711+caerKEA@users.noreply.github.com>
2020-10-05 13:27:31 +0200

    Testing a getReservationByPhonenumber

--- src/test/java/kino/xp/project/Repository/ReservationRepositoryTest.java
+++ src/test/java/kino/xp/project/Repository/ReservationRepositoryTest.java
@@ -2,7 +2,10 @@ package kino.xp.project.Repository;
 
 import kino.xp.project.Model.Reservation;
 import kino.xp.project.Service.ReservationService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,12 +14,14 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ReservationRepositoryTest {
 
     @Autowired
     ReservationService rs;
 
     @Test
+    @Order(1)
     public void canCreateReservation(){
         //Arrange
         ArrayList<Reservation> list = new ArrayList<>();
@@ -36,18 +41,30 @@ class ReservationRepositoryTest {
     }
 
     @Test
+    @Order(2)
     public void canAddReservationToDatabase()
     {
-        Reservation r = new Reservation(0, "Hans", "Jensen", 15151515,
+        Reservation r = new Reservation(0, "Hans", "Jensen", 98989898,
                 "hansJ@mail.dk", "Tenet", "19:30", "2020-11-30",
                 "2020-10-15", 2, 24, true);
         assertTrue(rs.createReservation(r));
     }
 
     @Test
+    @Order(3)
+    void canReadReservationByPhonenumber()
+    {
+        Reservation r = rs.getReservationByPhonenumber(98989898);
+        assertNotNull(r);
+    }
+
+    @Test
+    @Order(4)
     void canDeleteReservationFromDatabase()
     {
-        Reservation newR = rs.getReservationByPhonenumber(15151515);
+        Reservation newR = rs.getReservationByPhonenumber(98989898);
         assertTrue(rs.deleteReservation(newR.getReservation_id()));
     }
+
+
 }
